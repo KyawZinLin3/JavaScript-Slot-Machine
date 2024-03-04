2f1246329af40a112b1e7853325ddbced797d8d2
KyawZinLin3 <kyawzinlin14future@gmail.com>
2024-03-04 21:55:39 +0630

    spin done

--- project.js
+++ project.js
@@ -72,13 +72,22 @@ const spin = () => {
     }
   }
 
-  const reels = [[], [], []];
+  const reels = [];
   for (let i = 0; i < COLS; i++) {
-    for (let j = 0; j < ROWS; j++) {}
+    reels.push([]);
+    const reelSymbols = [...symbols];
+    for (let j = 0; j < ROWS; j++) {
+      const randomIndex = Math.floor(Math.random() * reelSymbols.length);
+      const selectedSymbol = reelSymbols[randomIndex];
+      reels[i].push(selectedSymbol);
+      reelSymbols.splice(randomIndex, 1);
+    }
   }
+  return reels;
 };
 
-spin();
+const reels = spin();
+console.log(reels);
 
 let balance = deposit();
 console.log("balance : " + balance);
